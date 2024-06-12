9ceccdfa8588a77aa8b23efc8af1d52d091eba07
Randi Øyri <randi.oyri@digdir.no>
2024-06-12 13:11:39 +0200

    ID-4182: Miljøavhengige mapper

--- docker/overrideProperties.sh
+++ docker/overrideProperties.sh
@@ -2,21 +2,9 @@
 # ${ENVIRONMENT} must be configured in idporten-cd in container.env
 
 # copy eidas.xml
-ENV_SOURCE=/etc/config/profiles/${ENVIRONMENT}/eidas.xml
-OVERRIDE_FILE=/etc/config/eidas.xml
-if [ -f "$ENV_SOURCE" ]; then
-    if [ -f "$OVERRIDE_FILE" ]; then
-        rm -rf "$OVERRIDE_FILE"
-    fi
-    mv "$ENV_SOURCE" "$OVERRIDE_FILE"
-fi
-
-# copy whitelist metadata file
-ENV_SOURCE=/etc/config/profiles/${ENVIRONMENT}/metadata/MetadataFetcher_Service.properties
-OVERRIDE_FILE=/etc/config/metadata/MetadataFetcher_Service.properties
-if [ -f "$ENV_SOURCE" ]; then
-    if [ -f "$OVERRIDE_FILE" ]; then
-        rm -rf "$OVERRIDE_FILE"
-    fi
-    mv "$ENV_SOURCE" "$OVERRIDE_FILE"
+ENV_SOURCE=/etc/config/profiles/${ENVIRONMENT}/
+DEFAULT_CONFIG=/etc/config/
+if [ -d "$ENV_SOURCE" ]; then
+    echo "Copy files for environment ${ENVIRONMENT}" && ls -lt
+    cp -r "$ENV_SOURCE"** "$DEFAULT_CONFIG"
 fi
