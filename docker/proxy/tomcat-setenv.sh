50642002a0c391a6458fdf1d82b12dcb892ddfd3
Randi Øyri <randi.oyri@digdir.no>
2024-07-01 11:05:02 +0200

    ID-4499: Java security-provider for luna added

--- docker/proxy/tomcat-setenv.sh
+++ docker/proxy/tomcat-setenv.sh
@@ -1,8 +1,12 @@
 
-# bouncycastle. 
+# Bouncycastle java security provider
 export JAVA_OPTS="$JAVA_OPTS -Djava.security.properties=/opt/java/openjdk/conf/security/java_bc.security"
 export JAVA_OPTS="$JAVA_OPTS --module-path /usr/local/lib/bcprov-jdk18on-1.78.jar"
 export JAVA_OPTS="$JAVA_OPTS --add-modules org.bouncycastle.provider"
+
+# Luna java security provider for HSM
+export JAVA_OPTS="$JAVA_OPTS --module-path /var/usrlocal/luna/jsp/LunaProvider.jar"
+export JAVA_OPTS="$JAVA_OPTS --add-modules Luna"
 
 # eidas config
 export EIDAS_PROXY_CONFIG_REPOSITORY="/etc/config/eidas-proxy/"
